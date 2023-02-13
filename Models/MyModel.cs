be6705a67458317f4041e29428168fa73edbdcd8
Nguyễn Văn Phú <phunv@ftech.ai>
2023-02-13 17:21:07 +0700

    add config

--- Models/MyModel.cs
+++ Models/MyModel.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json;
-
-namespace Analyze.DesktopApp.Models
+﻿namespace Analyze.DesktopApp.Models
 {
     public class ProfileModel
     {
@@ -26,5 +24,30 @@ namespace Analyze.DesktopApp.Models
     {
         public string phone { get; set; }
         public string message { get; set; }
+    }
+
+    public class TicketModel
+    {
+        public string symbol { get; set; }
+        public float priceChange { get; set; }
+        public float priceChangePercent { get; set; }
+        public float weightedAvgPrice { get; set; }
+        public float prevClosePrice { get; set; }
+        public float lastPrice { get; set; }
+        public float lastQty { get; set; }
+        public float bidPrice { get; set; }
+        public float bidQty { get; set; }
+        public float askPrice { get; set; }
+        public float askQty { get; set; }
+        public float openPrice { get; set; }
+        public float highPrice { get; set; }
+        public float lowPrice { get; set; }
+        public float volume { get; set; }
+        public float quoteVolume { get; set; }
+        public long openTime { get; set; }
+        public long closeTime { get; set; }
+        public long firstId { get; set; }
+        public long lastId { get; set; }
+        public long count { get; set; }
     }
 }
