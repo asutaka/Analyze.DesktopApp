9663cf4b611e96a74d311af025d6592a20269105
phunv@ftech.ai <phunv@ftech.ai>
2023-02-22 21:40:45 +0700

    fix bug get Data

--- Job/RecallErrorSymbolJob.cs
+++ Job/RecallErrorSymbolJob.cs
@@ -15,44 +15,51 @@ namespace Analyze.DesktopApp.Job
     {
         public void Execute(IJobExecutionContext context)
         {
-            var settings = Program.Configuration.GetSection("API").Get<APIModel>();
-            var lError = StaticVal.lstError;
-            foreach (var item in lError)
+            try
             {
-                try
+                var settings = Program.Configuration.GetSection("API").Get<APIModel>();
+                var lError = StaticVal.lstError.ToList();
+                foreach (var item in lError)
                 {
-                    var content = StaticClass.GetWebContent(string.Format(settings.History, item)).GetAwaiter().GetResult();
-                    if (!string.IsNullOrWhiteSpace(content))
+                    try
                     {
-                        var time = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-                        var arr = JArray.Parse(content);
-                        StaticVal.dic1H.Add(item, arr.Select(x => new LocalTicketModel
+                        var content = StaticClass.GetWebContent10s(string.Format(settings.History, item)).GetAwaiter().GetResult();
+                        if (!string.IsNullOrWhiteSpace(content))
                         {
-                            name = item.ToLower(),
-                            e = (long)x[0],
-                            o = float.Parse(x[1].ToString()),
-                            h = float.Parse(x[2].ToString()),
-                            l = float.Parse(x[3].ToString()),
-                            c = float.Parse(x[4].ToString()),
-                            v = float.Parse(x[5].ToString()),
-                            q = float.Parse(x[7].ToString()),
-                            state = true,
-                            ut = time
-                        }));
+                            var time = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                            var arr = JArray.Parse(content);
+                            StaticVal.dic1H.Add(item, arr.Select(x => new LocalTicketModel
+                            {
+                                name = item.ToLower(),
+                                e = (long)x[0],
+                                o = float.Parse(x[1].ToString()),
+                                h = float.Parse(x[2].ToString()),
+                                l = float.Parse(x[3].ToString()),
+                                c = float.Parse(x[4].ToString()),
+                                v = float.Parse(x[5].ToString()),
+                                q = float.Parse(x[7].ToString()),
+                                state = true,
+                                ut = time
+                            }));
 
-                        StaticVal.lstError.Remove(item);
+                            StaticVal.lstError.Remove(item.ToString());
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        NLogLogger.PublishException(ex, $"RecallErrorSymbolJob.Execute|EXCEPTION|INPUT: {JsonConvert.SerializeObject(item)}| {ex.Message}");
                     }
                 }
-                catch (Exception ex)
+
+                if (StaticVal.lstError.Count() < 10)
                 {
-                    NLogLogger.PublishException(ex, $"RecallErrorSymbolJob.Execute|EXCEPTION|INPUT: {JsonConvert.SerializeObject(item)}| {ex.Message}");
+                    //stop job
+                    StaticVal.jobError.Pause();
                 }
             }
-
-            if(StaticVal.lstError.Count() < 10)
+            catch(Exception exm)
             {
-                //stop job
-                StaticVal.jobError.Interrupt();
+                NLogLogger.PublishException(exm, $"RecallErrorSymbolJob.Execute|EXCEPTION| {exm.Message}");
             }
         }
     }
